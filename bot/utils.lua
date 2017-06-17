1a1070fbe7a4d225402a632bfe270ce78f6ecfb1
3pehrdev <sepehrsalehi0111@gmail.com>
2017-06-17 15:46:04 +0430

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -1281,6 +1281,7 @@ function get_receiver_api(msg)
 end
 
 function send_api_msg(msg, receiver, text, disable_web_page_preview, markdown,inline_text,inline_url)
+	local config = loadfile ("./data/config.lua")()
  local url_api = 'https://api.telegram.org/bot'.._config.api_key..'/sendMessage?chat_id='..receiver..'&text='..URL.escape(text)
   if disable_web_page_preview == true then
     url_api = url_api..'&disable_web_page_preview=true'
