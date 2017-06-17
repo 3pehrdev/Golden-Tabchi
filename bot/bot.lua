f7464bed88b7ee7ee57dd613012902985c58a6d1
3pehrdev <sepehrsalehi0111@gmail.com>
2017-06-17 14:37:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -222,10 +222,11 @@ function load_config( )
     print("Sudo user: " .. user)
   end
   return config
+end
+
  local config = loadfile ("./data/config.lua")()
   for v,user in pairs(config.api_token) do
     print("API Helper Token: " .. user)
-  end
   return config
 end
 -- Create a basic config.json file and saves it.
